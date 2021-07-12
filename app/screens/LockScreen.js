37d9a28f502a5e0bd91683062c47b5989248522a
1kl1 <mamy0320@naver.com>
2021-07-12 20:41:47 +0900

    bugfix

--- app/screens/LockScreen.js
+++ app/screens/LockScreen.js
@@ -11,71 +11,71 @@ let dc = new Date("2021-07-01T14:13:00.000");
 // `YYYY-MM-DDTHH:mm:ss.sss`
 
 function WelcomeScreen({ navigation }) {
-	
+
 	let [spin, setSpin] = useState(0);
 
 	useInterval(() => {
 		setSpin(spin + 1);
 	}, 150);
-	
+
 	let now = new Date();
-	var diff = parseInt((now-dc)/1000);
+	var diff = parseInt((now - dc) / 1000);
 	let diff_day = parseInt(diff / 86400);
 	diff -= diff_day * 86400;
-	let diff_hour = parseInt(diff/3600);
+	let diff_hour = parseInt(diff / 3600);
 	diff -= diff_hour * 3600
-	let diff_min = parseInt(diff/60);
+	let diff_min = parseInt(diff / 60);
 	diff -= diff_min * 60;
 	let diff_sec = diff;
-	
-	diff_hour = ("0"+(diff_hour.toString())).slice(-2);
-	diff_min = ("0"+(diff_min.toString())).slice(-2);
-	diff_sec = ("0"+(diff_sec.toString())).slice(-2);
-	
+
+	diff_hour = ("0" + (diff_hour.toString())).slice(-2);
+	diff_min = ("0" + (diff_min.toString())).slice(-2);
+	diff_sec = ("0" + (diff_sec.toString())).slice(-2);
+
 	return (
 		<View style={styles.background}>
-			<Topbar/>
-			
+			<Topbar />
+
 			<Image
 				style={styles.headerBackground}
-				source={require("../assets/backgroundHeader.png")}/>
+				source={require("../assets/backgroundHeader.png")} />
 			<Image
 				style={styles.headerBackgroundDeco}
-				source={require("../assets/backgroundHeaderDeco.png")}/>
-			
+				source={require("../assets/backgroundHeaderDeco.png")} />
+
 			<View style={styles.headerBar}>
-				<View style={{flexDirection:"row", marginVertical: 2.5}}>
+				<View style={{ flexDirection: "row", marginVertical: 2.5 }}>
 					<Text style={styles.headerbarText}>설치일시 : </Text>
 					<Text style={styles.headerbarDate}>21.03.09 15:54</Text>
 				</View>
-				<View style={{flexDirection:"row", marginVertical: 2.5}}>
+				<View style={{ flexDirection: "row", marginVertical: 2.5 }}>
 					<Text style={styles.headerbarText}>차단일시 : </Text>
 					<Text style={styles.headerbarDate}>21.07.01 14:13</Text>
 				</View>
 			</View>
-			
+
 			<View style={styles.loadingContainer}>
 				<Image
-				  style={[styles.loading, {transform: [{rotate: (spin*30)+'deg'}]}]}
-				  source={require("../assets/loading.png")} />
+					style={[styles.loading, { transform: [{ rotate: (spin * 30) + 'deg' }] }]}
+					source={require("../assets/loading.png")} />
 			</View>
 			<Image
 				style={styles.cameraIcon}
-				source={require("../assets/cameraBlock.png")}/>
-			
+				source={require("../assets/cameraBlock.png")} />
+
 			<View style={styles.timerContainer}>
 				<Text style={styles.timerText}>{diff_day + " "}일</Text>
-				<Text style={styles.timerText}>{diff_hour+" : "+diff_min+" : "+diff_sec}</Text>
+				<Text style={styles.timerText}>{diff_hour + " : " + diff_min + " : " + diff_sec}</Text>
 			</View>
-			
+
 			<View style={styles.flexEnd}>
-				
-				<Image source={require("../assets/button.png")} style={styles.allowBtn}/>
-			  <Text style={styles.title}>카메라 허용</Text>
-				
+
+				<Image source={require("../assets/button.png")} style={styles.allowBtn} />
+				<Text style={styles.title}>카메라 허용</Text>
+
 				<View style={styles.versionBar}>
-					<Text style={{color:"#fff", marginVertical:14}}>Version</Text>
-					<Text style={{color:"#fff"}}> 2.0.03</Text>
+					<Text style={{ color: "#fff", marginVertical: 14 }}>Version</Text>
+					<Text style={{ color: "#fff" }}> 2.0.03</Text>
 				</View>
 			</View>
 		</View>
@@ -83,17 +83,17 @@ function WelcomeScreen({ navigation }) {
 }
 
 
-const bottomHeight = 170;
+const bottomHeight = 210;
 // 한바퀴에 1.8초
 const styles = StyleSheet.create({
-	title:{
+	title: {
 		color: "#fff",
 		fontSize: 15,
 		fontWeight: "bold",
-		position:"absolute",
-		bottom:70
+		position: "absolute",
+		bottom: 70
 	},
-	allowBtn:{
+	allowBtn: {
 		width: "95%",
 		height: "10%",
 		resizeMode: "contain",
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
 	},
 	timerContainer: {
 		position: "absolute",
-		bottom: bottomHeight+50,
+		bottom: bottomHeight + 50,
 		justifyContent: "center",
 		alignItems: "center"
 	},
@@ -109,14 +109,14 @@ const styles = StyleSheet.create({
 		color: "#fff",
 		fontSize: 16,
 	},
-	versionBar:{
-		backgroundColor:"#141617",
+	versionBar: {
+		backgroundColor: "#141617",
 		width: "100%",
 		alignItems: "center",
 		flexDirection: "row",
 		justifyContent: "center",
 	},
-	flexEnd:{
+	flexEnd: {
 		flex: 1,
 		width: "100%",
 		height: 20,
@@ -125,11 +125,11 @@ const styles = StyleSheet.create({
 	},
 	headerBar: {
 		position: "absolute",
-		top: "10%",
+		top: "11.5%",
 		marginTop: 20
 	},
 	headerbarDate: {
-		fontWeight: "regular",
+		fontWeight: "normal",
 		color: "#fff",
 		fontSize: 22,
 		marginTop: 1
@@ -141,13 +141,13 @@ const styles = StyleSheet.create({
 	},
 	background: {
 		flex: 1,
-		justifyContent: "flex",
+		justifyContent: "flex-end",
 		alignItems: "center",
 		backgroundColor: "#252525",
 	},
 	headerBackground: {
 		width: "100%",
-		height: 200,
+		height: 220,
 	},
 	headerBackgroundDeco: {
 		width: "100%",
@@ -166,7 +166,7 @@ const styles = StyleSheet.create({
 	},
 	cameraIcon: {
 		position: "absolute",
-		bottom: bottomHeight+26,
+		bottom: bottomHeight + 26,
 		width: 190,
 		height: 190
 	}
